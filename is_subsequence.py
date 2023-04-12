346d7b051cba5ef4c1a332b543471e2f180dc25b
Felipe Limeira <limeira.felipe94@gmail.com>
2023-04-11 21:46:15 -0300

    ex4 is_subsenquence

---
+++ is_subsequence.py
@@ -0,0 +1,43 @@
+
+"""
+    Given two strings s and t, return true if s is a subsequence of t, or false otherwise.
+    A subsequence of a string is a new string that is formed from the original string by deleting some (can be none) 
+    of the characters without disturbing the relative positions of the remaining characters. 
+    (i.e., "ace" is a subsequence of "abcde" while "aec" is not).
+
+    Exemplo 1:
+        Input: s = "abc", t = "ahbgdc"
+        Output: true
+        
+    Exemplo 2:
+        Input: s = "axc", t = "ahbgdc"
+        Output: false
+"""
+
+def isSubsequence(s, t):
+    if len(s) > len(t):
+        return False
+    count = 0
+    if s == "":
+        return True
+    for i in range(len(s)):
+        if s[i] in t:
+            count += 1
+            t = t[t.index(s[i])+1:]
+            if count == len(s):
+                return True
+        else:
+            return False
+
+
+def test_abc():
+    assert isSubsequence("abc", "ahbgdc") == True
+    
+def test_axc():
+    assert isSubsequence("axc", "ahbgdc") == False
+
+def test_aec():
+    assert isSubsequence("aec", "abcde") == False
+
+def test_vazio():
+    assert isSubsequence("", "abcde") == True
